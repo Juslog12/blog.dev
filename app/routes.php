a47d344ebc6cfbbb9fdd59390787051feec2cf05
Justin <Justin@Justins-MacBook-Pro.local>
2014-06-26 13:49:54 -0500

    Created a route for roll dice

--- app/routes.php
+++ app/routes.php
@@ -27,3 +27,56 @@ Route::get('/portfolio', function()
 {
     return "This is my portfolio";
 });
+
+
+Route::get('/', function()
+{
+	return View::make('temp.my-first-view');
+});
+
+
+Route::get('/contacts/{id}', function($contacId)
+{
+	return "Contact: $contactId";
+});
+
+
+Route::get('/contacts/{id}', function($contacId)
+{
+	return View::make('contacts.show')->with('contact_id',$contactId);
+});
+
+
+Route::get('/sayhello/{name}', function($name)
+{
+    if ($name == "Chris")
+    {
+        return Redirect::to('/');
+    }
+    else
+    {
+        return View::make('temp.my-first-view')->with('name',$name);
+
+    }
+});
+//second way of doing above
+
+Route::get('/sayhello/{name}', function($name)
+{
+    if ($name == "Chris")
+    {
+        return Redirect::to('/');
+    }
+    else
+    {
+        $data = array('name' => $name);
+        return View::make('temp.my-first-view')->with($data);
+        
+    }
+});
+
+Route::get('/rolldice', function()
+{
+    return "Lets roll dice!";
+});
+
