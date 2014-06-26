dc4545ea149bee8fed7977d8f8caba43499fd037
Justin <Justin@Justins-MacBook-Pro.local>
2014-06-26 11:31:50 -0500

    Added back the home page route

--- app/routes.php
+++ app/routes.php
@@ -11,8 +11,19 @@
 |
 */
 
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+
+Route::get('/resume', function()
+{
+    return "This is my resume";
+});
+
 
 Route::get('/portfolio', function()
 {
-    return "This is my portfolio.";
+    return "This is my portfolio";
 });
