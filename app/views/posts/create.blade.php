dcc5ce3e4feae31fdec94116f0f0fc073163eada
Justin <Justin@Justins-MacBook-Pro.local>
2014-07-01 10:58:07 -0500

    Updated create to add validation messages

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -10,16 +10,19 @@
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 
 <h1>Create a New Blog Post</h1>
+
+{{ $errors->first('title', '<span class="help-block">:message</span>') }}
 <form role="form" action={{{ action('PostsController@store') }}} method="POST">
 
 <div class="form-group">
 <label for="title">Title</label>
-<input class="form-control" type="text" id="title" name="title" value="{{{ Input::old('title') }}}" placeholder="Enter Title">
+<input class="form-control" type="text" id="title" name="title" value="{{{ Input::old('title') }}}" placeholder="Enter a title">
 </div>
 
+{{ $errors->first('body', '<span class="help-block">:message</span>') }}
 <div class="form-group">
 <label for="body">Body</label>
-<textarea class="form-control" id="body" name="body">{{{ Input::old('body') }}}</textarea>
+<textarea class="form-control" id="body" name="body" placeholder="Enter text here">{{{ Input::old('body') }}}</textarea>
 <br>
 
 <input type="submit">
