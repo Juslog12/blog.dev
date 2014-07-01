39cc10fd10fd70dcab4413a4142410e64a916757
Justin <Justin@Justins-MacBook-Pro.local>
2014-07-01 15:07:47 -0500

    updated index to have edit buttons

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -6,14 +6,35 @@
 @stop 
   
 @section('content')
-<h1>All the Posts</h1>
+<html>
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<h2>All Posts</h2>
+<table class="table table-hover" align= "center" border = black 1px solid>
+ <th>Title</th>
+ <th>Action</th>
 
  @foreach ($posts as $post)
- 	{{link_to_action('PostsController@show', $post->title, array($post->id))}}<br>	
+ <tr>
+ 	<td>
+ 	{{link_to_action('PostsController@show', $post->title, array($post->id))}}	
+ 	</td>
+ 	<td>
+ 	{{link_to_action('PostsController@edit', 'Edit', array($post->id), array('class' =>'btn btn-default btn-sm'))}}
+ 	</td>
+ </tr>	
  @endforeach
+ </table>
+ 
  <hr>
- {{link_to_action('PostsController@create', 'New Post')}}
+ {{link_to_action('PostsController@create', 'New Post')}}<br>
+ <br>
+ <br>
+{{ $posts->links() }}
 @stop
+
+</html>
 
 @section('bottomscript')
 @stop
