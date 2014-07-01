b9a6ce21955cc9bfb48bbe92a45fccabe4969759
Justin <Justin@Justins-MacBook-Pro.local>
2014-07-01 09:56:39 -0500

    Created a form to create new post with body and title

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -4,19 +4,28 @@
 @stop 
   
 @section('content')
-<h1>Create a New Blog Post</h1>
-<form action={{{ action('PostsController@store') }}} method="POST">
-<label for="title">Title</label>
-<input type="text" id="title" name="title" value="{{{ Input::old('title') }}}">
-<br>
+<html>
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 
+<h1>Create a New Blog Post</h1>
+<form role="form" action={{{ action('PostsController@store') }}} method="POST">
+
+<div class="form-group">
+<label for="title">Title</label>
+<input class="form-control" type="text" id="title" name="title" value="{{{ Input::old('title') }}}" placeholder="Enter Title">
+</div>
+
+<div class="form-group">
 <label for="body">Body</label>
-<textarea id="body" name="body">{{{ Input::old('body') }}}</textarea>
+<textarea class="form-control" id="body" name="body">{{{ Input::old('body') }}}</textarea>
 <br>
 
 <input type="submit">
 </form>
 
+</html>
 
 @stop
 
