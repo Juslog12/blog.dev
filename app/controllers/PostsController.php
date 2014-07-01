43035c6686b39b5d809595c5fa606072df951891
Justin <Justin@Justins-MacBook-Pro.local>
2014-07-01 11:01:09 -0500

    Updated postcontroller to add validator for posts

---
+++ app/controllers/PostsController.php
@@ -0,0 +1,107 @@
+<?php
+
+class PostsController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//return "Show a list of all posts";	
+ 		return View::make('posts.index')->with('posts', Post::all());
+	}
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		
+		//Log::info('Someone is trying to create a post.');
+		return View::make('posts.create');
+	}	
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		
+		$validator = Validator::make(Input::all(), Post::$rules);
+
+		if ($validator->fails())
+	    {
+	       return Redirect::back()->withInput()->withErrors($validator); 
+	    }
+	    else
+	    {
+		    $post = new Post();
+			$post->title = Input::get('title');
+			$post->body = Input::get('body');
+			$post->save();
+			return Redirect::action('PostsController@index');
+	    }
+
+		
+	}
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//return "Show a specific post";
+		$post = Post::find($id);
+		return View::make('posts.show')->with('post', $post);
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		return "Show a form for editing a specific post";
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+
+
+
+
+}
