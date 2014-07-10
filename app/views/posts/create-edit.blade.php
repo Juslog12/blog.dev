b5d7e306677bb689a614865765a0e14ccc6e911b
Justin <Justin@Justins-MacBook-Pro.local>
2014-07-10 10:27:14 -0500

    Added thumbnail to tile of posts

--- app/views/posts/create-edit.blade.php
+++ app/views/posts/create-edit.blade.php
@@ -4,13 +4,16 @@
 @stop 
   
 @section('content')
+
 	
 	@if(isset($post))
 	<h2>Edit Post</h2>
-	{{ Form::model($post, array('action' => array('PostsController@update', $post->id), 'method' => 'PUT')) }}
+	<h4>{{{ $post->created_at->format('l, F jS Y @ h:i:s A') }}}</h4>
+	{{ Form::model($post, array('action' => array('PostsController@update', $post->id), 'method' => 'PUT', 'files' => true)) }}
 	@else
 	<h2>Create a New Post</h2>
-	{{ Form::open(array('action' => 'PostsController@store')) }}
+	
+	{{ Form::open(array('action' => 'PostsController@store', 'files' => true)) }}
 	@endif
 
 		{{ $errors->first('title', '<span class="help-block">:message</span>') }}
@@ -23,6 +26,10 @@
 		<div class="form-group">
 		{{ Form::label('body', 'Body') }}
 		{{ Form::textarea('body') }}	
+		</div>
+		<div>
+			{{ Form::label('image', 'Image') }}
+			{{ Form::file('image') }}
 		</div>
 
 		<input type="submit">
